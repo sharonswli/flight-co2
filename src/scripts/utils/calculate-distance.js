c52b554d2eb60bf6c3acc665b7197b6c937da231
Sharon <sharonsuetwli@gmail.com>
2017-09-09 13:49:40 -0700

    Implement get total distance without offset added. Testing

--- src/scripts/utils/calculate-distance.js
+++ src/scripts/utils/calculate-distance.js
@@ -21,28 +21,48 @@ function getDistanceCorrection(distance) {
   return distanceCorrectionMap[type];
 }
 
+Math.radians = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+
 function getGreaterCircleDistance(airportA, airportB) {
-  console.log("airportA: ", airportA);
-  console.log("airportB: ", airportB);
+  var EARTH_RADIUS = 6371e3; // Earth's radius in meters
+  
+  var latA = Math.radians(airportA.lat);
+  var latB = Math.radians(airportB.lat);
+
+  var latDiff = Math.radians(airportB.lat - airportA.lat);
+  var longDiff = Math.radians(airportB.long - airportA.long);
+
+  var a = Math.sin(latDiff/2) * Math.sin(latDiff/2) + Math.cos(latA) * Math.cos(latB) * Math.sin(longDiff/2) * Math.sin(longDiff/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return EARTH_RADIUS * c;
 }
 
-// @Input (airports: string[])
+// @Input (airports: { id: string, lat: number, long: number }[]) 
 // @Output (distance: number)
 function getTotalDistance(airports) {
-  // airports should implement { lat: number , long: number } where number is 6 decimal places
-
-  airports.reduce(function(sum, value, index, rawArr) {
-    // Check if last item in array
+  return airports.reduce(function(sum, value, index, rawArr) {
+    // Check if last item in collection
     if(index === rawArr.length - 1) {
       return sum;
     }
-    var gcd = getGreaterCircleDistance(value, airports[index + 1]);
-    return sum + gcd;
-  }, 0)
+    var gcdAB = getGreaterCircleDistance(value, rawArr[index + 1]);
+    console.log(`gcd between ${value.id} and ${rawArr[index + 1].id} is: `, gcdAB);
+    // get distance correction
+    // var offset = getDistanceCorrection(gcdAB);
+    return sum + gcdAB;
+    // return sum + gcdAB + offset;
+  }, 0);
 }
+
 console.log("Route 1");
-getTotalDistance([ { id: 'YVR', lat: 49.193889, long: -123.184444 }, { id: 'CDG', lat: 49.012779, long: 2.55 } ]);
+var route1 = getTotalDistance([ { id: 'YVR', lat: 49.193889, long: -123.184444 }, { id: 'CDG', lat: 49.012779, long: 2.55 } ]);
+console.log("route1 total distance: ", route1);
 console.log("Route 2");
-getTotalDistance([ { id: 'YVR', lat: 49.193889, long: -123.184444 }, { id: 'YYZ', lat: 43.677223, long: -79.630556 }, { id: 'CDG', lat: 49.012779, long: 2.55 } ]);
+var route2 = getTotalDistance([ { id: 'YVR', lat: 49.193889, long: -123.184444 }, { id: 'YYZ', lat: 43.677223, long: -79.630556 }, { id: 'CDG', lat: 49.012779, long: 2.55 } ]);
+console.log("route2 total distance: ", route2);
 console.log("Route 3");
-getTotalDistance([ { id: 'YVR', lat: 49.193889, long: -123.184444 }, { id: 'YYZ', lat: 43.677223, long: -79.630556 }, { id: 'CDG', lat: 49.012779, long: 2.55 }, { id: 'HKG', lat: 22.308919, long: 113.914603 } ]);
+var route3 = getTotalDistance([ { id: 'YVR', lat: 49.193889, long: -123.184444 }, { id: 'YYZ', lat: 43.677223, long: -79.630556 }, { id: 'CDG', lat: 49.012779, long: 2.55 }, { id: 'HKG', lat: 22.308919, long: 113.914603 } ]);
+console.log("route3 total distance: ", route3);
