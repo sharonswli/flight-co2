a93fe934c2c5e2c6c233f38ba55611fb13b12937
Sharon <sharonsuetwli@gmail.com>
2017-09-09 17:52:41 -0700

    Test: boilerplate comes with .babelrc - can try using ES6 syntax like imports/exports

---
+++ src/scripts/utils/test.js
@@ -0,0 +1,5 @@
+import { getTotalDistance } from './calculate-distance';
+
+
+const test = getTotalDistance([ { id: 'YVR', lat: 49.193889, long: -123.184444 }, { id: 'YYC', lat: 51.113888, long: -114.020278 } ]);
+console.log(test);
