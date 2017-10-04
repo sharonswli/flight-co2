87a1f8ce8ce46b9724f3ef9f81936518e5d7715c
stephchuolee <schuolee@gmail.com>
2017-10-04 00:07:39 -0700

    Style popup. Colours can be changed, footer can be removed if desired

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -33,11 +33,11 @@ storage.get('color', function(resp) {
 var template = () => {
   return (`
   <div class="action-container">
-    <ul>
-      <li><a href="" data-label="CO2">Default</a></li>
-      <li><a href="" data-label="a banana">Bananas</a></li>
-      <li><a href="" data-label="a year's worth of wine">Year's worth of wine</a></li>
-      <li><a href="" data-label="average North American">Average North American</a></li>
+    <ul class="dropdown list-unstyled">
+      <li class="dropdown-item active"><a href="" data-label="CO2">CO2 Emissions</a></li>
+      <li class="dropdown-item"><a href="" data-label="a banana">Bananas</a></li>
+      <li class="dropdown-item"><a href="" data-label="a year's worth of wine">A year of wine</a></li>
+      <li class="dropdown-item"><a href="" data-label="average North American">Average North American</a></li>
     </ul>
   </div>
   `);
@@ -63,8 +63,19 @@ popup.addEventListener("click", function(e) {
 
     e.preventDefault();
     var relateToLabel;
+    var dropdownItems = document.querySelectorAll('.dropdown-item');
     if (e.target.getAttribute("data-label")) {
       relateToLabel = e.target.getAttribute("data-label");
+      // Remove active class for siblings
+      for (var i = 0; i < dropdownItems.length; i++) {
+        if (dropdownItems[i].classList.contains('active')) {
+          dropdownItems[i].classList.remove('active');
+          break;
+        }
+      }
+      // Add active class to selected dropdown item
+      e.target.parentElement.classList.add('active')
+
       ext.tabs.query({active: true, currentWindow: true}, function(tabs) {
         var activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, { action: 'set-attribute', data: relateToLabel });
